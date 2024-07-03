87a6bfbb36edff9513c1edab082f1e370193ad70
Danil Demin <prodbykngns@gmail.com>
2024-07-03 15:10:56 +0300

    Added classNames into the Panel component

--- src/shared/ui/panel/Panel.tsx
+++ src/shared/ui/panel/Panel.tsx
@@ -1,13 +1,17 @@
+import classNames from "classnames";
 import styles from "./styles.module.scss"
 import { ReactNode } from 'react';
 
 type TPanelProps = {
     children: ReactNode;
+    className?: string;
 }
 
-export const Panel = ({children}: TPanelProps) => {
+export const Panel = (props: TPanelProps) => {
+const {children, className} = props;
+
     return (
-        <div className={ styles.panel }>
+        <div className={ classNames(styles.panel, className) }>
             {children}
         </div>
     );
