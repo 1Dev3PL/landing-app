f3ed13feb721f51c762367a779532f420172ac93
Danil Demin <prodbykngns@gmail.com>
2024-07-03 14:44:03 +0300

    Added panel component and placed svg icons in icons folder

--- src/pages/main-page/ui/MainPage.tsx
+++ src/pages/main-page/ui/MainPage.tsx
@@ -1,7 +1,8 @@
 import { Header } from 'widgets/header'
 import { Content } from '../../../shared/ui/content/Content'
 import styles from './MainPage.module.scss'
-import {Footer} from 'widgets/footer';
+import {Footer} from 'widgets/footer'
+import { Panel } from 'shared/ui/panel/Panel'
 
 export function MainPage() {
   return (
