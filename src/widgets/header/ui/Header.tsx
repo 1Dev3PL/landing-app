1fdf8d238c00e1096eade57abd18caff0a299b21
Danil Demin <prodbykngns@gmail.com>
2024-07-04 11:53:50 +0300

    Refactored Header component

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -4,18 +4,18 @@ import { Link } from "react-router-dom"
 
 export const Header = () => {
     return (
-        <div className={ styles.main_container }>
-            <div className="logo">
-                <img src={ logo } alt="petraterra_logo" className="logo" />
-            </div>
-            <div className={ styles.navigation }>
-                <Link to='' className={ styles.nav_link }>Продукция</Link>
-                <Link to='' className={ styles.nav_link }>О компании</Link>
-                <Link to='' className={ styles.nav_link }>Контакты</Link>
-                <Link to='' className={ styles.nav_link }>Контакты</Link>
-            </div>
-            <div className={ styles.phone_number_container }>
-                <p className= { styles.phone_number }>+7 (921) 592-02-81</p>
+        <div className={ styles.container }>
+            <div className={ styles.header }>
+                <img src={ logo } alt="logo" className={ styles.logo } />
+                <div className={ styles.nav_container }>
+                    <Link to='' className={ styles.nav_link }>Продукция</Link>
+                    <Link to='' className={ styles.nav_link }>О компании</Link>
+                    <Link to='' className={ styles.nav_link }>Контакты</Link>
+                    <Link to='' className={ styles.nav_link }>Контакты</Link>
+                </div>
+                <div className={ styles.phone_number }>
+                    +7 (921) 592-02-81
+                </div>
             </div>
         </div>
     )
