0b535d288b525f03ab5780454470d731aa37b11b
Danil Demin <prodbykngns@gmail.com>
2024-07-05 11:36:22 +0300

    Solved merge conflinc in MainPage.tsx

--- src/pages/main-page/ui/MainPage.tsx
+++ src/pages/main-page/ui/MainPage.tsx
@@ -1,23 +1,11 @@
-<<<<<<< HEAD
-import { Header } from 'widgets/header'
-import { Content } from 'shared/ui/content/Content'
-import styles from './MainPage.module.scss'
-import {Footer} from 'widgets/footer'
-import { Panel } from 'shared/ui/panel/Panel'
-import { MainPageHeading } from 'widgets/main-page-heading/ui/MainPageHeading'
-=======
 import {Header} from 'widgets/header'
 import styles from './styles.module.scss'
 import {Footer} from 'widgets/footer';
->>>>>>> b3010988138ccf214439bfb5f9207b7a8b927a6f
 
 export function MainPage() {
   return (
     <div>
       <Header />
-      <Content>
-        <MainPageHeading />
-      </Content>
       <Footer/>
     </div>
   )
